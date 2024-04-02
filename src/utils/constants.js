74997bcef04b43da917f92806be08f55cdf8414e
jimmytalanoa <138366901+jimmytalanoa@users.noreply.github.com>
2024-04-02 16:01:52 +1100

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,4 @@
 export const PAGE_SIZE = 10;
 export const oneStopTokenUrl = "https://developer-auth.1-stop.biz/oauth2/token";
 export const oneStopTrackingEndpoint = "https://connect.1-stop.biz/tracking";
-export const BASE_64 = `MjBtMWRncnQ0OGU1ZmZ0YnBpOWQxdWMwcDg6amo1MHB0MTdyNjJobWZjdXB1NjM3ajdzbnU1bW1obWhzZzZ2YWpmMDE0cGUybjljcWZj`;
-export const apiKey = `KwE9DvuoUBaz5Vq8g0fqI6EpisfGearb1ORqhIS0`;
 
-const clientSecret = `jj50pt17r62hmfcupu637j7snu5mmhmhsg6vajf014pe2n9cqfc`;
-const clientId = `20m1dgrt48e5fftbpi9d1uc0p8`;
