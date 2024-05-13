ac6f9faaf76661b144c5143eef110812e55bcca2
jimmytalanoa <jimmytalanoa@gmail.com>
2024-05-13 13:58:45 +1000

    added rathco parser

---
+++ src/utils/creds.js
@@ -0,0 +1,37 @@
+export const BASE_64 = `MjBtMWRncnQ0OGU1ZmZ0YnBpOWQxdWMwcDg6amo1MHB0MTdyNjJobWZjdXB1NjM3ajdzbnU1bW1obWhzZzZ2YWpmMDE0cGUybjljcWZj`;
+export const apiKey = `KwE9DvuoUBaz5Vq8g0fqI6EpisfGearb1ORqhIS0`;
+
+export const adobeCredentials = {
+  client_credentials: {
+    client_id: "adbe07d35ecf46ac9060d310b4812013",
+    client_secret: "p8e-VDQrWkSxU1fCS9S9Isc3oNrIXD-qPoV-",
+  },
+  service_principal_credentials: {
+    organization_id: "48F91E2A65E51ECD0A495F9F@AdobeOrg",
+  },
+};
+
+export const rathcoCredentials = {
+  client_credentials: {
+    client_id: "11hRA7QlJMNvDULzTLM9rAwUm",
+    client_secret: "oNRM0bc6jwSiwxdIsrN57Xz9wD6jgR4fmFiTdHW59Lw8Fs5X",
+  },
+  client_UUID: "0f1e6904-470c-4cfa-9883-700b2b95111c",
+};
+
+export const effectiveCredentials = {
+  tenants: [
+    {
+      id: "5f854b65-d914-4427-9059-03c7ac1f2d05",
+      name: "Effective Logistics",
+    },
+  ],
+};
+
+export const rathCoCredentials = {
+  client_credentials: {
+    client_id: "mqOpxcfp4Bp94ssAGMhmBwWYB",
+    client_secret: "O54s3Zbd7dDh74rzI5hl4Gibd4e0IgVX34YVXRF3Lu5aKhoT",
+  },
+  client_UUID: "baae613a-79e0-44e9-95fb-e458b7410f48",
+};
